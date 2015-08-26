45c85bb56b274dd1f6118970c2f58bb667c56e81
Jamaal Sedayao <you@example.com>
2015-08-26 14:29:31 -0400

    updates code fails

--- unit-1-final-project/GOTimerViewController.m
+++ unit-1-final-project/GOTimerViewController.m
@@ -13,6 +13,12 @@
 
 @property (nonatomic) NSTimer *timer;
 @property (nonatomic) Exercises *thisExercise;
+
+@property (weak, nonatomic) IBOutlet UIButton *endButton;
+@property (weak, nonatomic) IBOutlet UIButton *pauseButton;
+@property (weak, nonatomic) IBOutlet UIButton *continueButton;
+
+
 @end
 
 @implementation GOTimerViewController
@@ -21,15 +27,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.continueButton.hidden = YES;
 
     NSLog(@"Exercise Index: %lu", self.currentExerciseIndex);
-
-    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0
-                                                  target:self
-                                                selector:@selector(updateExerciseTimer)
-                                                userInfo:nil
-                                                 repeats:YES];
     
+//timer starts when viewLoads
+    [self initializeTimer];
+    
+//setting fonts
+    
+    self.totalTimeExerciseLabel.font = [UIFont fontWithName:@"NikeTotal90" size:15.0];
+    self.exerciseNameLabel.font = [UIFont fontWithName:@"NikeTotal90" size:15.0];
+    self.exerciseTimeLabel.font = [UIFont fontWithName:@"NikeTotal90" size:15.0];
+
     
     self.thisExercise = [self.currentWorkout.exercises objectAtIndex: (NSUInteger)self.currentExerciseIndex];
     self.currentExerciseName = self.thisExercise.nameOfExercise;
@@ -43,6 +54,7 @@
     NSString *imageName = self.thisExercise.exerciseImageString;
     UIImageView *view = [[UIImageView alloc]initWithImage:[UIImage imageNamed:imageName]];
     self.exerciseImageView.image = view.image;
+   
     
     if (self.currentExerciseIndex == (NSInteger)nil){
         self.currentExerciseIndex = 0;
@@ -51,38 +63,98 @@
     
     
 }
+- (void) initializeTimer {
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0
+                                                  target:self
+                                                selector:@selector(updateExerciseTimer)
+                                                userInfo:nil
+                                                 repeats:YES];
+    
+    
+}
+
+//method pushes to next exercise in array
+- (void) initializeNextExerciseScreen {
+    
+    GOTimerViewController *nextViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WorkoutController"];
+    nextViewController.currentWorkout = self.currentWorkout;
+    nextViewController.currentExerciseIndex = self.currentExerciseIndex+1;
+    [self.timer invalidate];
+    [self.navigationController pushViewController:nextViewController animated:YES];
+    
+}
+
 
 - (void) updateExerciseTimer {
     NSInteger count = [self.currentWorkout.exercises count];
-    NSLog(@"%lu", count);
-    NSLog(@"%f", self.currentExerciseTime);
-    NSInteger one = 1.0;
-    NSInteger currentExerciseTime = [self.exerciseTimeLabel.text integerValue];
-    NSInteger nextNumber = currentExerciseTime - one;
-    self.exerciseTimeLabel.text = [NSString stringWithFormat:@"%lu", nextNumber];
+    
+    NSDate *currentTime = [[NSDate alloc]init];
+    NSDate *mainTime;
+    
+    NSTimeInterval currentExerciseTime = [self.exerciseTimeLabel.text integerValue];
+    
+    NSTimeInterval elapsedTime = [currentTime timeIntervalSinceDate:mainTime];
+    
+    mainTime -= elapsedTime;
+    
+
+    
+    float one = 1.0;
+    float currentExerciseTime = [self.exerciseTimeLabel.text integerValue];
+    float nextNumber = currentExerciseTime - one;
+    NSLog(@"Time left: %f", nextNumber);
+    
+    //self.exerciseTimeLabel.text = [NSString stringWithFormat:@"%f", self.nextNumber];
+    
+    NSDate *timeDate = [NSDate dateWithTimeIntervalSince1970:nextNumber];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"mm : ss : SS"];
+    [dateFormatter setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0.0]];
+    NSString *timeString = [dateFormatter stringFromDate:timeDate];
+    self.exerciseTimeLabel.text = timeString;
+    
+    
+
+
     if (nextNumber == 0 && self.currentExerciseIndex < count - 1) {
         NSLog(@"Next exercise");
-        GOTimerViewController *nextViewController = [self.storyboard instantiateViewControllerWithIdentifier:@"WorkoutController"];
-        nextViewController.currentWorkout = self.currentWorkout;
-        nextViewController.currentExerciseIndex = self.currentExerciseIndex+1;
-        [self.timer invalidate];
-        [self.navigationController pushViewController:nextViewController animated:YES];
-    }
-    
-    else if (nextNumber == 0 && self.currentExerciseIndex == count - 1) {
-        //        if (self.currentExerciseIndex == count){
-        //            [self.timer invalidate];
+        
+        [self initializeNextExerciseScreen];
+        
+    } else if (nextNumber == 0 && self.currentExerciseIndex == count - 1) {
+        
         [self.timer invalidate];
         [self.navigationController popToRootViewControllerAnimated:YES];
-        //        }
     }
     
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)pausePressed:(UIButton *)sender {
+    
+    [self.timer invalidate];
+    
+    self.pauseButton.hidden = YES;
+    self.continueButton.hidden = NO;
+    
 }
+- (IBAction)continuePressed:(UIButton *)sender {
+    [self initializeTimer];
+    
+    self.pauseButton.hidden = NO;
+    self.continueButton.hidden = YES;
+    
+}
+
+- (IBAction)end:(UIButton *)sender {
+    [self.timer invalidate];
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+
+
+
 
 /*
  #pragma mark - Navigation
